fd92fd7f15467a7d84a7cbded2238784b1903b1e
PaulSPuntusov <pps_ru@mail.ru>
2024-09-24 22:01:25 +0300

    сделал

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<String> list = new ArrayList<>(Arrays.asList("Будет","так","как","ты","захочешь"));
+        Stream<String> stream = list.stream();
+        System.out.println(getStringFromStream(stream));
+    }
+    public static String getStringFromStream(Stream stringStream){
+        return stringStream.collect(Collectors.joining(" ")).toString();
     }
 }
